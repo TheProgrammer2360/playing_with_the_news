93962f78a487e84e4fdfa13d6874af985a7e0402
TheProgrammer <mxolisinkosi2360@gmail.com>
2023-04-10 08:32:03 +0200

    made sure that the request will always take news from current day and news from the previous day

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+import datetime
 import requests
+from datetime import date
 import os
-
+# -------------------------------------------- PARAMETERS -----------------------------------------------------------
 API_KEY = os.getenv("API_KEYS")
-parameters = {"apiKey": API_KEY, "country": "us"}
-ENDPOINT = "https://newsapi.org/v2/top-headlines"
+ENDPOINT = "https://newsapi.org/v2/everything"
+# ----------------------------------------GETTING THE CORRECT DATE ---------------------------------------------------
+# getting today's date with correct format
+today = date.today()
+today_with_correct_format = today.strftime("%Y-%m-%d")
+# getting yesterday's date with correct format
+yesterday = date.today()
+yesterday -= datetime.timedelta(days=1)
+yesterday_with_correct_format = yesterday.strftime("%Y-%m-%d")
+# ----------------------------------------- REQUEST -------------------------------------------------------------------
+parameters = {"apiKey": API_KEY, "q": "apple", "from": yesterday_with_correct_format, "to": today_with_correct_format}
 request = requests.get(url=ENDPOINT, params=parameters)
 request.raise_for_status()
+# getting hold of that number of data that we have
+data = request.json()
+print(data["totalResults"])
 
